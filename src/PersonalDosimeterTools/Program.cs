a275a6a4019c043c9c7a69d93f6a79bf8443ddf7
HyunIl <khi0526@knou.ac.kr>
2022-02-24 21:42:53 +0900

    create new project

---
+++ src/PersonalDosimeterTools/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PersonalDosimeterTools;
+
+internal class Program
+{
+	[STAThread]
+	public static void Main()
+	{
+		var app = new App();
+		app.InitializeComponent();
+		app.Run();
+	}
+}

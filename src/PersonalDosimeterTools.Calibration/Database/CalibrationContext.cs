f4973eb1b7239f719d094c510df10098a2fa1ae8
HyunIl <khi0526@knou.ac.kr>
2022-03-04 20:57:54 +0900

    add CalibrationContext

---
+++ src/PersonalDosimeterTools.Calibration/Database/CalibrationContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalDosimeterTools.Calibration.Models;
+
+namespace PersonalDosimeterTools.Calibration.Database;
+
+public class CalibrationContext : DbContext
+{
+	private readonly string _path;
+	public DbSet<Isotope> Isotopes { get; set; }
+	public DbSet<RadioactiveSource> RadioactiveSources { get; set; }
+	public DbSet<MeasurementEnvironment> MeasurementEnvironments { get; set; }
+	public DbSet<MeasurementResult> MeasurementResults { get; set; }
+	public DbSet<ReferenceDosimeter> ReferenceDosimeters { get; set; }
+
+	public CalibrationContext(string path)
+	{
+		_path = path;
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		=> optionsBuilder.UseSqlite($"Data Source={_path}");
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		
+	}
+}

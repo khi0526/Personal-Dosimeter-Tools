a06730fba24ad43c1fc1503823ba803465b745d6
HyunIl <khi0526@knou.ac.kr>
2022-03-05 02:38:19 +0900

    change ReferenceDosimeter.CalibrationValue type
    double? to double

--- src/PersonalDosimeterTools.Calibration/Models/ReferenceDosimeter.cs
+++ src/PersonalDosimeterTools.Calibration/Models/ReferenceDosimeter.cs
@@ -9,7 +9,7 @@ public class ReferenceDosimeter
 	public string IdentificationCode { get; set; }
 	public DosimeterType Type { get; set; }
 	public string? Nickname { get; set; }
-	public double? CalibrationValue { get; set; }
+	public double CalibrationValue { get; set; }
 
 	public ICollection<MeasurementEnvironment> MeasurementEnvironments { get; } = new ObservableCollection<MeasurementEnvironment>();
 }
